c569bf66dd1600561796c17f7281079535c30a1d
Amresh Maurya <amresh.terminal@gmail.com>
2023-06-20 20:50:05 +0530

    backend: starting of backend

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import productAPI from './api/productAPI.js'
+import cors from 'cors'
+const app = express()
+
+// load json
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.send(productAPI)
+})
+
+
+const port = 5000
+
+app.listen(port,()=>{
+    console.log(`server started at http://localhost:${port}`)
+})
